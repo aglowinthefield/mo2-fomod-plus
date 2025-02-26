0e026f494a2175d2ad4cb8a2690df94fa28e0c78
Pascal <46202211+Horf@users.noreply.github.com>
2025-02-26 20:16:12 +0100

    Update fomod_plus_installer_de.ts

--- installer/fomod_plus_installer_de.ts
+++ installer/fomod_plus_installer_de.ts
@@ -4,54 +4,66 @@
 <context>
     <name>FomodInstallerWindow</name>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="196"/>
+        <location filename="FomodInstallerWindow.cpp" line="194"/>
         <source>Installieren</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="198"/>
-        <location filename="FomodInstallerWindow.cpp" line="367"/>
+        <location filename="FomodInstallerWindow.cpp" line="196"/>
+        <location filename="FomodInstallerWindow.cpp" line="370"/>
         <source>Weiter</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="289"/>
+        <location filename="FomodInstallerWindow.cpp" line="288"/>
         <source>Name:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="290"/>
+        <location filename="FomodInstallerWindow.cpp" line="289"/>
         <source>Autor:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="291"/>
+        <location filename="FomodInstallerWindow.cpp" line="290"/>
         <source>Version:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="292"/>
+        <location filename="FomodInstallerWindow.cpp" line="291"/>
         <source>Webseite:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="358"/>
+        <location filename="FomodInstallerWindow.cpp" line="357"/>
         <source>Manuell</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="359"/>
+        <location filename="FomodInstallerWindow.cpp" line="358"/>
         <source>Alle Vorherige Optionen Ausw&auml;hlen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="366"/>
+        <location filename="FomodInstallerWindow.cpp" line="360"/>
+        <location filename="FomodInstallerWindow.cpp" line="586"/>
+        <source>Bilder Ausblenden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="FomodInstallerWindow.cpp" line="369"/>
         <source>Zur&uuml;ck</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FomodInstallerWindow.cpp" line="368"/>
+        <location filename="FomodInstallerWindow.cpp" line="371"/>
         <source>Abbrechen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="FomodInstallerWindow.cpp" line="360"/>
+        <location filename="FomodInstallerWindow.cpp" line="590"/>
+        <source>Bilder Einblenden</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
